f23e84663c0920ae2e5025a08e4abac6ae1470a8
asandoz <alain.sandoz@cea.fr>
2025-11-19 15:57:32 +0100

    feat: add setup script for linux

---
+++ setup_dev.sh
@@ -0,0 +1,9 @@
+sudo apt-get install libsdl2-dev
+dpkg -L libsdl2-dev | grep SDL.h
+
+sudo apt-get install libsdl2-image-dev
+dpkg -L libsdl2-image-dev | grep SDL.h
+
+echo "Compiling"
+g++ src/*.cpp `sdl2-config --cflags --libs` -lSDL2_image -o bin/main
+echo "Compiled"
