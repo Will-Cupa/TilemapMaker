7d710cb67341ef91a55c5b6104e7651d87212b63
Will-Cupa <adjm.sandoz@outlook.fr>
2025-11-19 16:22:35 +0100

    feat: add bin folder creation  on setup

--- setup_dev.sh
+++ setup_dev.sh
@@ -4,6 +4,8 @@ dpkg -L libsdl2-dev | grep SDL.h
 sudo apt-get install libsdl2-image-dev
 dpkg -L libsdl2-image-dev | grep SDL.h
 
+mkdir -p bin
+
 echo "Compiling"
 g++ src/*.cpp `sdl2-config --cflags --libs` -lSDL2_image -o bin/main
 echo "Compiled"
